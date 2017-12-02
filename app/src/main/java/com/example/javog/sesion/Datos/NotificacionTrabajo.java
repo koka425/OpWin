abacb946b3512fb1f65535912bdb35c3fa99e027
koka425 <javogx@hotmail.com>
2017-12-02 11:23:40 -0600

    new java, xml, style changes, fab

---
+++ app/src/main/java/com/example/javog/sesion/Datos/NotificacionTrabajo.java
@@ -0,0 +1,15 @@
+package com.example.javog.sesion.Datos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.javog.sesion.R;
+
+public class NotificacionTrabajo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificacion_trabajo);
+    }
+}
---
+++ app/src/main/java/com/example/javog/sesion/TerminarTrabajo.java
@@ -0,0 +1,13 @@
+package com.example.javog.sesion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TerminarTrabajo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminar_trabajo);
+    }
+}
